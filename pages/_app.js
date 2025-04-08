e922124d71d8dc239b8efffffded4c63fb942d32
Arnab Roy <mr.roy.arnab@gmail.com>
2025-04-08 14:08:14 +0530

    feat: giveaway winners claim

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import '../styles/global.css'; // Make sure this path is correct
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function MyApp({ Component, pageProps }) {
     return (
         <div>
+            <GoogleOAuthProvider clientId='1072886259148-0pijp3g4009q2885au3uc9koqbt62tqg.apps.googleusercontent.com'>
             <Head>
                 <title>PiBook</title>
                 <link rel="icon" href="/Logo.svg" />
             </Head>
             
             <Component {...pageProps} />
+            </GoogleOAuthProvider>
         </div>
     )
 }
